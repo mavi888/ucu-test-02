8bdf5d45ffe83bbb66b592ad53580e3474b3c83f
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2020-05-19 11:29:55 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import awsConfig from './aws-config';
+
 function App() {
+  const [showResult, setShowResult] = useState(false);
+  const [apiMessage, setApiMessage] = useState("");
+  const [name, setName] = useState("");
+
+  const wasGreeted = async () => {
+    console.log(name);
+
+    const response = await fetch(`${awsConfig.endpoint}hello?name=${name}`, {
+      mode: 'cors'
+    });
+    
+    const responseData = await response.text();
+    console.log(responseData)
+
+    setShowResult(true);
+    setApiMessage(responseData);  
+  };
+
+  const greet = async () => {
+    await fetch(`${awsConfig.endpoint}hello?name=${name}`, {
+      method: 'POST',
+      mode: 'cors'
+    });
+
+    setShowResult(true);
+    setApiMessage(`Hola ${name}`); 
+
+  }
+
+  const handleChange = (e) => {  
+    setName(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Greeter</h1>
+        <input type="text"  onChange={handleChange}/>
+        <button onClick={wasGreeted}>Was greeted?</button>
+        <button onClick={greet}>Greet</button>
+        <div>
+          {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
+        </div>
       </header>
     </div>
   );
